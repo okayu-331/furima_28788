07a2af0c021e1b845207734d9465d13e2edcf201
okayu-331 <kuma.fuyuno@gmail.com>
2020-08-16 16:46:47 +0900

    userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i }
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ンヴー]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ンヴー]+\z/ }
+  validates :birthday, presence: true
 end
