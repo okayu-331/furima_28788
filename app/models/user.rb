5cb520b14231ceef9b7ce79247935ab804f388b5
okayu-331 <kuma.fuyuno@gmail.com>
2020-08-16 17:37:09 +0900

    バリデーション修正
    長音の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i }
-  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥ー]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥ー]+\z/ }
   validates :family_name_kana, presence: true, format: { with: /\A[ァ-ンヴー]+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ンヴー]+\z/ }
   validates :birthday, presence: true
