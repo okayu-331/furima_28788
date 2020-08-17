b9e005aeaf11c6cce22e0cf51de13438798b7637
okayu-331 <kuma.fuyuno@gmail.com>
2020-08-17 23:47:12 +0900

    FactoryBotの準備

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name { "アイテムの名前-sample123/" } 
+    description { "説明のテキスト-sample123/" }
+    category_id { Faker::Number.within(range: 1..10) }
+    status_id { Faker::Number.within(range: 1..6) }
+    shipping_fee_id { Faker::Number.within(range: 1..2) }
+    shipping_origin_id { Faker::Number.within(range: 1..47) }
+    shipping_days_id { Faker::Number.within(range: 1..3) }
+    price { Faker::Number.within(range: 300..9999999) }
+    association :user
   end
 end
