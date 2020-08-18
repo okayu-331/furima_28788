8c3580fb090d1f6921dfac6d3d05b2dba2b073a6
okayu-331 <kuma.fuyuno@gmail.com>
2020-08-18 16:28:50 +0900

    createアクションの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, only: :new
+
   def index
   end
   
@@ -7,5 +9,25 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(
+      :name, :description, :category_id, :status_id, :shipping_fee_id, 
+      :shipping_origin_id, :shipping_days_id, :price, :image
+    ).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
