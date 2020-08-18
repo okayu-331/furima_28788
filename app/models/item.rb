d6258dff34c82b11c6fd199b81f27d56affd7c9e
okayu-331 <kuma.fuyuno@gmail.com>
2020-08-18 21:46:28 +0900

    itemモデル バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,19 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_fee
   belongs_to_active_hash :shipping_origin
   belongs_to_active_hash :shipping_days
+
+  with_options presence: true do
+    with_options format: { with: /\A.+\z/ } do
+      validates :name
+      validates :description
+    end
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :shipping_origin_id
+    validates :shipping_days_id
+    validates :price, numericality: { only_integer: true, message: "positive number and half-width characters only" }
+    validates :image
+  end
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "out of setting range"}
 end
