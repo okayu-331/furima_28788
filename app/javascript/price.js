34774b148a69d4859692dd13eb8468a178319f37
okayu-331 <kuma.fuyuno@gmail.com>
2020-08-21 21:21:26 +0900

    価格手数料の自動計算の修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -6,12 +6,12 @@ window.addEventListener('DOMContentLoaded', () => {
   if(path === "/items/new" || path === "/items" || editRegex.test(path) || updateRegex.test(path)) {
     const price = document.getElementById('item-price');
     const salesCommission = document.getElementById('add-tax-price');
-    const commissionFeeRate = 10;
+    const commissionFeeRate = 10; //販売手数料10%
     const profit = document.getElementById('profit');
   
     price.addEventListener('input', function() {
       salesCommission.textContent = Math.floor(this.value / commissionFeeRate).toLocaleString();
-      profit.textContent = Math.floor(this.value - parseInt(salesCommission.textContent, 10)).toLocaleString();
+      profit.textContent = Math.floor(this.value * 0.9).toLocaleString(); //利益90％
     })
   }
 })
