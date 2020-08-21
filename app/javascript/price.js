8a459aa3fc1dc6f407746b9055db012494a7609d
okayu-331 <kuma.fuyuno@gmail.com>
2020-08-21 17:18:34 +0900

    価格手数料の自動計算修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,14 +1,17 @@
 window.addEventListener('DOMContentLoaded', () => {
-  const path = location.pathname
-  if(path === "/items/new" || path === "/items") {
+  const path = location.pathname;
+  const editRegex = /^\/items\/\d+\/edit$/
+  const updateRegex = /^\/items\/\d+$/
+
+  if(path === "/items/new" || path === "/items" || editRegex.test(path) || updateRegex.test(path)) {
     const price = document.getElementById('item-price');
     const salesCommission = document.getElementById('add-tax-price');
     const commissionFeeRate = 10;
     const profit = document.getElementById('profit');
   
     price.addEventListener('input', function() {
-      salesCommission.textContent = Math.floor(this.value / commissionFeeRate);
-      profit.textContent = Math.floor(this.value - parseInt(salesCommission.textContent, 10));
+      salesCommission.textContent = Math.floor(this.value / commissionFeeRate).toLocaleString();
+      profit.textContent = Math.floor(this.value - parseInt(salesCommission.textContent, 10)).toLocaleString();
     })
   }
 })
