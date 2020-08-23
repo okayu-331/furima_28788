875745548a841d66696027fdb5421aac0d6b05a2
okayu-331 <kuma.fuyuno@gmail.com>
2020-08-23 14:22:25 +0900

    FactoryBotの準備

---
+++ spec/factories/item_purchases.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :item_purchase do
+    postal_code { "123-4567" } 
+    prefecture_id { Faker::Number.within(range: 1..47) }
+    city { "テスト市" }
+    address { "テスト町1-2ー３" }
+    building { "テストビル1-2ー１０１" }
+    phone_number { Faker::Number.leading_zero_number(digits: 11) }
+    purchase { 1 }
+    association :user
+    association :item
+  end
+end
