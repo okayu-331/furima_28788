cc8444c4e7875800172ec77e9c51bbb13f70f680
okayu-331 <kuma.fuyuno@gmail.com>
2020-08-24 16:50:46 +0900

    バリデーション設定

--- app/forms/item_purchase.rb
+++ app/forms/item_purchase.rb
@@ -3,7 +3,13 @@ class ItemPurchase
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :purchase_id, :user_id, :item_id
 
-  # バリデーション
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
+    validates :prefecture_id
+    validates :city 
+    validates :address 
+    validates :phone_number, format: { with: /\d{10,11}/ }
+  end
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
