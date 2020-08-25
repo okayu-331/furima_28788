a04c74d30e1e15c57bf087cc48feda1009d751d5
okayu-331 <kuma.fuyuno@gmail.com>
2020-08-25 16:10:07 +0900

    クレジット決済機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -13,6 +13,7 @@ class PurchasesController < ApplicationController
   def create
     @purchase = ItemPurchase.new(purchase_params)
     if @purchase.valid?
+      pay_item
       @purchase.save
       redirect_to root_path
     else
@@ -26,10 +27,19 @@ class PurchasesController < ApplicationController
     params.require(:item_purchase).permit(
       :postal_code, :prefecture_id, :city, :address, :building,
       :phone_number, :purchase_id
-    ).merge(user_id: current_user.id, item_id: params[:item_id])
+    ).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: purchase_params[:token],  # カードトークン
+      currency:'jpy'  # 通貨の種類
+    )
   end
 end
--- app/forms/item_purchase.rb
+++ app/forms/item_purchase.rb
@@ -1,7 +1,7 @@
 class ItemPurchase
 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :purchase_id, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :purchase_id, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
@@ -9,6 +9,7 @@ class ItemPurchase
     validates :city 
     validates :address 
     validates :phone_number, format: { with: /\d{10,11}/ }
+    validates :token
   end
 
   def save
