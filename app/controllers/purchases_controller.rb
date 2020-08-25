c9a56e86593956a7d6bb325db74a8ac538505503
okayu-331 <kuma.fuyuno@gmail.com>
2020-08-25 17:45:54 +0900

    indexアクションの修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,13 +1,10 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item, only: [:index, :create]
+  before_action :move_to_root, only: :index
 
   def index
     @purchase = ItemPurchase.new
-
-    if @item.purchase != nil || current_user.id == @item.user.id
-      redirect_to root_path
-    end
   end
 
   def create
@@ -23,15 +20,21 @@ class PurchasesController < ApplicationController
 
   private
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_root
+    if @item.purchase != nil || current_user.id == @item.user.id
+      redirect_to root_path
+    end
+  end
+
   def purchase_params
     params.require(:item_purchase).permit(
       :postal_code, :prefecture_id, :city, :address, :building,
       :phone_number, :purchase_id
     ).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
-  end
-
-  def set_item
-    @item = Item.find(params[:item_id])
   end
 
   def pay_item
